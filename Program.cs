70eba1fe9e07eb45cb8330653750c6f38d862063
gabrielioliveira19 <54965416+gabrielioliveira19@users.noreply.github.com>
2019-09-05 21:01:36 -0300

    Numero bissexto em C-sharp
    Atividade para verificar se o número é bissexto ou não

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano;
+
+
+
+
+            Console.WriteLine("Informo o ano:");
+            ano = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+            if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))
+            {
+                Console.WriteLine("é bissexto:");
+            }
+
+            else
+                Console.WriteLine("Não é bissexto:");
+
+        }
+    }
+}
